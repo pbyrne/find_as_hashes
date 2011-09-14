695dc5ad7a440625c88349007c9cfcf852870765
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-09-14 09:21:54 -0500

    Create the initial methods
    Tests and such to come.

--- lib/find_as_hashes.rb
+++ lib/find_as_hashes.rb
@@ -1,5 +1,13 @@
 require "find_as_hashes/version"
 
-module FindAsHashes
-  # Your code goes here...
+module ActiveRecord
+  module FinderMethods
+    def all_as_hashes
+      connection.select_all(to_sql)
+    end
+
+    def first_as_hash
+      connection.select_one(to_sql)
+    end
+  end
 end
